3e20ddb7cc3acedfeff24f965816c80f87ab7450
AnushkaNagar07 <nagar.anushka07@gmail.com>
2026-06-16 19:21:01 +0530

    Implement sum calculation for first N natural numbers
    Fix the program to correctly calculate the sum of first N natural numbers.

--- Day 1/Q1.py
+++ Day 1/Q1.py
@@ -1,1 +1,6 @@
-Day 01
+# Write a program to Calculate sum of first N natural numbers.
+N=int (input("Enter the number:"))
+sum=0
+for i in range(1,N+1):
+  sum=sum+i
+print("The desired sum is :",N)
