5738b53a3f3aa4fd9ff2766e643aec4e1713b80b
AnushkaNagar07 <nagar.anushka07@gmail.com>
2026-06-18 15:21:47 +0000

    add all the files

--- Day 1/Q3.py
+++ Day 1/Q3.py
@@ -1,1 +1,8 @@
 # Write a program to Find factorial of a number
+N=int (input("Enter a number:"))
+fact=1
+i=1
+while (i<=N):
+    fact=fact*i
+    i=i+1
+print(fact)
--- Day 1/Q4.py
+++ Day 1/Q4.py
@@ -1,1 +1,9 @@
 # Write a program to Count digits in a number
+N=int (input("Enter a number:"))
+count=0
+temp=N
+while N !=0:
+    if temp/10!=0:
+        count=count+1
+    else:
+        print(count)
